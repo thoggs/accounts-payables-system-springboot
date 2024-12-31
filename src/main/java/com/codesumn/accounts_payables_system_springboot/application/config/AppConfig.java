db328135f248d7d41e68fa75278cf18cf59f1d50
Thiago Rodrigues <thiagothogs@icloud.com>
2024-12-31 10:32:13 -0300

    feat: add application configuration classes for core setup
    - Introduced `AppConfig` with `@EnableAsync` and `@EnableScheduling` for async task handling and scheduling.
    - Added `CorsConfig` to enable global CORS configuration, allowing all origins, methods, and headers.
    - Implemented `EnvironConfig` for managing environment variables using `dotenv` with fallback to system variables.
    - Included `SecurityConfig` for Spring Security setup, integrating JWT authentication, custom exception handling, and stateless session management.

---
+++ src/main/java/com/codesumn/accounts_payables_system_springboot/application/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.codesumn.accounts_payables_system_springboot.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AppConfig {
+}
---
+++ src/main/java/com/codesumn/accounts_payables_system_springboot/application/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.codesumn.accounts_payables_system_springboot.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
