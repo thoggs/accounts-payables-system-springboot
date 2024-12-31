f942e349cd841086091cd87abd1cf8f7a2da5996
Thiago Rodrigues <thiagothogs@icloud.com>
2024-12-31 10:39:03 -0300

    feat: add custom exception handling and utility classes
    - Introduce `CustomAccessDeniedHandler` and `CustomAuthenticationHandler` for handling access and authentication exceptions in Spring Security.
    - Add `GlobalExceptionHandler` to centralize exception responses, handling various exceptions like `AuthenticationException`, `AccessDeniedException`, `ResourceNotFoundException`, and more.
    - Create custom exception classes: `CustomUnauthorizedException`, `CustomUserNotFoundException`, `EmailAlreadyExistsException`, `EnumValidationException`, and `ResourceNotFoundException`.
    - Add utility classes `SortParser` and DTOs `SortingParamDTO`, `FilterCriteriaParamDTO` for dynamic parsing of sorting and filtering parameters.
    - Define `RolesEnum` to standardize role types for stricter validation.

---
+++ src/main/java/com/codesumn/accounts_payables_system_springboot/shared/exceptions/errors/CustomUserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codesumn.accounts_payables_system_springboot.shared.exceptions.errors;
+
+public class CustomUserNotFoundException extends RuntimeException {
+    public CustomUserNotFoundException() {
+        super();
+    }
+}
---
+++ src/main/java/com/codesumn/accounts_payables_system_springboot/shared/exceptions/errors/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codesumn.accounts_payables_system_springboot.shared.exceptions.errors;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+        super();
+    }
+}
