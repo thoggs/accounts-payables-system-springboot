3f8956ab9c823c78c280056a31b92ab4439efc86
Thiago Rodrigues <thiagothogs@icloud.com>
2024-12-31 14:01:47 -0300

    style: fix indentation in main application class
    - Adjust the indentation in `AccountsPayablesSystemSpringbootApplication.java` for better code readability and consistency.
    - No functional changes introduced.

--- src/main/java/com/codesumn/accounts_payables_system_springboot/AccountsPayablesSystemSpringbootApplication.java
+++ src/main/java/com/codesumn/accounts_payables_system_springboot/AccountsPayablesSystemSpringbootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccountsPayablesSystemSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsPayablesSystemSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsPayablesSystemSpringbootApplication.class, args);
+    }
 
 }
